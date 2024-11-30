d07012ea44293a577e44f16ac1e12b3a4129ed1b
Chase Steenbock <chase.steenbock@gmail.com>
2024-11-30 14:59:19 -0600

    first commit

---
+++ Nezumi/Program.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Nezumi;
+
+Console.WriteLine("Press any button to start the task");
+Console.ReadKey();
+
+var task = new BackgroundTask(TimeSpan.FromMilliseconds(1));
+task.Start();
+
+Console.WriteLine("Press any button to stop the task");
+Console.ReadKey();
+
+await task.StopAsync();
